d856b104f909353457f062579e21c19fe40c0171
ddela-cr <ddela-cr@student.42.fr>
2015-12-29 14:23:04 +0100

    Implemented variable arguments (stdarg.h)

---
+++ includes/printf.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   printf.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ddela-cr <ddela-cr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/12/29 13:20:36 by ddela-cr          #+#    #+#             */
+/*   Updated: 2015/12/29 14:21:48 by ddela-cr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PRINTF_H
+# define PRINTF_H
+
+int		ft_printf(const char * restrict format, ...);
+int ft_count_params(const char * restrict str);
+
+#endif
---
+++ srcs/printf.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   printf.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ddela-cr <ddela-cr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/12/29 13:21:01 by ddela-cr          #+#    #+#             */
+/*   Updated: 2015/12/29 14:21:47 by ddela-cr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <printf.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <libft.h>
+
+int ft_printf(const char * restrict format, ...)
+{
+	int			nb_params;
+	va_list		ap;
+	int	i;
+
+	i = 0;
+	nb_params = ft_count_params(format);
+	if (nb_params == 0)
+		ft_putstr((char *)format);
+	va_start(ap, format);
+	while (i < nb_params)
+	{
+		printf("%d\n", va_arg(ap, int));
+		i++;
+	}
+	va_end(ap);
+	return (0);
+}
+
+int ft_count_params(const char * restrict str)
+{
+	int	i;
+	int	count;
+
+	i = 0;
+	count = 0;
+	while (str[i])
+	{
+		if (str[i] == '%')
+			count++;
+		i++;
+	}
+	return (count);
+}
